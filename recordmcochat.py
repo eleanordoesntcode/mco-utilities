62c83bdda88933afaa0ec4e91c8d93bd3e0cbabc
ellie is not coding in the slightest <166862894+eleanordoesntcode@users.noreply.github.com>
2024-08-28 11:30:59 +0200

    r

--- recordmcochat.py
+++ recordmcochat.py
@@ -12,7 +12,7 @@ async def requestmorelog():
 def recordchat(text):
     print(text)
     f = open("log.txt", mode="a+")
-    f.write(text)
+    f.write(text+"\n")
     f.close()
     return(None)
 
@@ -22,15 +22,18 @@ async def mainloop(lastchunk):
     newchunk = newchunk.replace("<br />", "")
     newchunk = html.unescape(newchunk)
     if newchunk != lastchunk:
+        print("")
         currdate = time.strftime('%Y-%m-%d %H:%M:%S')
-        recordchat(f"[{currdate}] New chunk recieved:\n{newchunk}\n")
+        recordchat(f"[{currdate}] New chunk recieved:\n{newchunk}")
         lastchunk = newchunk
+    else:
+        print(".", end ="")
     if time.time() - starttime < 2:
         time.sleep(2-(time.time() - starttime))
     return(newchunk)
 # we first initiate the log
 currdate = time.strftime('%Y-%m-%d %H:%M:%S')
-recordchat(f"[{currdate}] Starting logging\n")
+recordchat(f"[{currdate}] Starting logging")
 lastchunk = ""
 while True:
     lastchunk = asyncio.run(mainloop(lastchunk))
